442907e3c0df682035bb7d548ee08ba9ae5e0e7b
Maksym Cherniavskyi <morfey897@gmail.com>
2024-02-06 15:02:14 +0200

    Fixed 404 page

--- apps/app/src/app/not-found.tsx
+++ apps/app/src/app/not-found.tsx
@@ -5,7 +5,8 @@ export default async function Page() {
 		<main>
 			<section className='bg-white dark:bg-gray-900 '>
 				<div className='container flex items-center justify-center min-h-screen px-6 py-12 mx-auto'>
-					<NotFound />
+					<p>Page not found</p>
+					{/* <NotFound /> */}
 				</div>
 			</section>
 		</main>
