9fbf3460d01d20ddcad5ceeb17c8f1f458a1cb45
Maksym Cherniavskyi <morfey897@gmail.com>
2024-02-06 15:07:26 +0200

    Fixed deployment process

--- apps/app/src/app/not-found.tsx
+++ apps/app/src/app/not-found.tsx
@@ -1,12 +1,39 @@
-import NotFound from '@/views/notFound';
+import { HiOutlineArrowLongLeft } from 'react-icons/hi2';
+import Button from '@/components/Button';
+import { APP } from '@/routes';
 
 export default async function Page() {
 	return (
 		<main>
 			<section className='bg-white dark:bg-gray-900 '>
 				<div className='container flex items-center justify-center min-h-screen px-6 py-12 mx-auto'>
-					<p>Page not found</p>
-					{/* <NotFound /> */}
+					<div className='w-full '>
+						<div className='flex flex-col items-center max-w-lg mx-auto text-center'>
+							<p className='text-4xl font-medium text-blue-500 dark:text-blue-400'>
+								404
+							</p>
+							<h1 className='mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl'>
+								{'page.not_found.headline'}
+							</h1>
+							<p className='mt-4 text-gray-500 dark:text-gray-400'>
+								{'page.not_found.subheadline'}
+							</p>
+
+							<div className='flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto'>
+								<Button
+									message={'button.go_back'}
+									icon={<HiOutlineArrowLongLeft size={24} />}
+								/>
+
+								<Button<React.AnchorHTMLAttributes<HTMLAnchorElement>>
+									href={APP}
+									tag='link'
+									variant='primary'
+									message={'button.go_home'}
+								/>
+							</div>
+						</div>
+					</div>
 				</div>
 			</section>
 		</main>
