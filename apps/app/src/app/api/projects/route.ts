98c68a532c13a1447b961462129aa3be95eb9fa8
Maksym Cherniavskyi <morfey897@gmail.com>
2024-03-08 17:31:35 +0200

    Test 3

--- apps/app/src/app/api/projects/route.ts
+++ apps/app/src/app/api/projects/route.ts
@@ -5,9 +5,12 @@ import { EnumResponse } from '@/enums';
 import { doError, parseError } from '@/utils';
 import * as ErrorCodes from '@/errorCodes';
 import { getSession } from '@/nextRequest';
+import { cookies } from 'next/headers';
+import { COOKIES } from '@packages/config/dist';
 
 export async function GET(request: NextRequest) {
 	try {
+		
 		const user = await getUserFromSession(getSession(request));
 		if (!user) throw doError(ErrorCodes.USER_UNAUTHORIZED);
 
@@ -17,6 +20,10 @@ export async function GET(request: NextRequest) {
 	} catch (error) {
 		console.log('ERROR', error);
 		return NextResponse.json({
+			details: {
+				cookie1: cookies().get(COOKIES.SESSION)?.value,
+				cookie2: getSession(request),
+			},
 			status: EnumResponse.FAILED,
 			error: parseError(error),
 		});
