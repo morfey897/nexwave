7e5835939537d74bb582a2332d1bec4dc2f26219
Maksym Cherniavskyi <morfey897@gmail.com>
2024-03-08 18:17:59 +0200

    force dynamic

--- apps/app/src/app/api/projects/route.ts
+++ apps/app/src/app/api/projects/route.ts
@@ -7,12 +7,11 @@ import { getProjectsByUserId } from '@/models/project';
 import { EnumResponse } from '@/enums';
 import { doError, parseError } from '@/utils';
 import * as ErrorCodes from '@/errorCodes';
-import { cookies } from 'next/headers';
+import { getSession } from '@/headers';
 
 export async function GET(request: NextRequest) {
 	try {
-		// const all = cookies().getAll();
-		const user = await getUserFromSession(cookies().get('nw_session')?.value);
+		const user = await getUserFromSession(getSession());
 		if (!user) throw doError(ErrorCodes.USER_UNAUTHORIZED);
 
 		const projects = await getProjectsByUserId(user.id);
