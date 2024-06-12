c52e8ad503d3241752c62dce48af41a707684037
Maksym Cherniavskyi <morfey897@gmail.com>
2024-06-12 09:01:37 +0300

    Created styles folder

--- apps/wave/src/app/layout.tsx
+++ apps/wave/src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { getI18n } from '~utils/headers';
 import { generateViewport, getMetadata } from '~utils/seo';
-import './globals.css';
+import '~styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
